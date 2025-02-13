16af6352369da7acb64aa74f89431d1ff91aef55
KerimHandal2 <131467858+KerimHandal2@users.noreply.github.com>
2025-02-12 20:23:45 -0600

    mis avances en js
    js aplications

---
+++ JavaScript/01-Fundamentos/assets/js/primitivos.js
@@ -0,0 +1,33 @@
+
+let nombre = prompt('peter parker');
+console.log(nombre);
+
+nombre = 'ben parker';
+console.log(nombre);
+
+nombre = "tia may";
+nombre = `tia may`;
+
+console.log(typeof nombre);
+
+let esMarvel = false;
+console.log(typeof esMarvel);
+
+let edad = 33;
+console.log(typeof edad);
+
+edad = 33.001;
+console.log(typeof edad);
+
+let superPoder;
+console.log(typeof superPoder); // ???
+
+let soyNull = null;
+console.log(typeof soyNull); // ???
+
+let symboll = Symbol('a');
+let symbol2 = Symbol('a');
+
+console.log(typeof symboll);
+
+console.log(typeof symbol2);
